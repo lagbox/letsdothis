5c6f3a51dd752a1923c534f78d2288cdeea810a7
lagbox <lagbox@gmail.com>
2016-04-17 00:34:11 -0400

    Add Data Model and Migration

---
+++ database/migrations/2016_04_17_041315_create_data_table_migration.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataTableMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('data');
+    }
+}
