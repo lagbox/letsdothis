6cf9f282751bd32313ceeb5372565b3ed4ab173e
lagbox <lagbox@gmail.com>
2016-07-10 03:11:12 -0400

    FormRequests - UserRequest and add to base Request

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+    protected $updateMethods = ['PUT', 'PATCH'];
+
+    protected function appendUnique($key, $keyName = 'id')
+    {
+        if (in_array($this->method(), $this->updateMethods) && $param = $this->route($key)) {
+            if ($param instanceof Model) {
+                $rt = "{$param->getKey()},{$param->getKeyName()}";
+            } else {
+                $rt = "{$param},{$keyName}";
+            }
+
+            return ','. $rt;
+        }
+    }
+
+    protected function filledOnCreate()
+    {
+        // if we are updating
+        if (in_array($this->method(), $this->updateMethods)) {
+            return 'present';
+        } else {
+            return 'required';
+        }
+    }
 }
